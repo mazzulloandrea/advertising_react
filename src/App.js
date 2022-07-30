654ef1f5112c1d5d0d5e4c0da666544745e86d63
Andrea Mazzullo <andrea.mazzullo@spindox.it>
2022-07-30 17:22:32 +0200

    advertising react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import CloseButton from './components/closeButton';
+import Blur from './components/blur';
+import Quiz from './components/quiz';
 
 function App() {
+
+  function close() {
+    const anim = document.getElementById("animation");
+    if (anim.classList.value.includes('exit')) {
+      return;
+    } else {
+      anim.classList.toggle("exit");
+      anim.onanimationend = (evt => evt.target.style.right = '-25vw')
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <div id="animation" className="container">
+        <div className="closeContainer" >
+          <CloseButton closeAnimation={(evt) => close()} />
+        </div>
+        <div className="blurSection" onClick={() => window.open("http://www.google.it")}>
+          <Blur />
+        </div>
+        <div className="quizSection" onClick={() => window.open("http://www.google.it")}>
+          <Quiz />
+        </div>
+      </div>
     </div>
   );
 }
