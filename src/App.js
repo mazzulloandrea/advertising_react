20e8e8660d2c33c4ac56bf7b3242dee782209fcd
Andrea Mazzullo <andrea.mazzullo@spindox.it>
2022-07-31 10:14:08 +0200

    add state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import CloseButton from './components/closeButton';
 import Blur from './components/blur';
 import Quiz from './components/quiz';
 
 function App() {
+  const [closeAnimation, setCloseAnimation] = useState(null);
 
-  function close() {
-    console.log('close');
-    const anim = document.getElementById("animation");
-    if (anim.classList.value.includes('exit')) {
-      return;
-    } else {
+  useEffect(() => { }, []);
+
+  useEffect(() => {
+    if (closeAnimation && closeAnimation !== 'open') {
+      console.log(closeAnimation);
+    }
+    if (closeAnimation === 'close') {
+      const anim = document.getElementById("animation");
       anim.classList.toggle("exit");
       anim.onanimationend = (evt => evt.target.style.right = '-100vw')
     }
-  };
+  }, [closeAnimation]);
 
   return (
     <div id="app">
-      <div id="animation" className="container">
+      <div id="animation" className="container" onAnimationEnd={() => setCloseAnimation('open')}>
         <div className="closeContainer" >
-          <CloseButton closeAnimation={(evt) => close()} />
+          <CloseButton closeAnimation={() => setCloseAnimation('close')} />
         </div>
         <div className="blurSection" onClick={() => window.open("http://www.google.it")}>
           <Blur />
